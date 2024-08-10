921b2932cbd8d876582959234c042522551d27a0
Iudà <86917292+IUDA194@users.noreply.github.com>
2024-08-10 16:54:36 +0200

    redis crandeteils update

--- cash_plan/cash_plan/settings.py
+++ cash_plan/cash_plan/settings.py
@@ -134,7 +134,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.getenv('REDIS_CONNECTION_URL'),
+        "LOCATION": f"redis://{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/{os.getenv('REDIS_DB')}",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
